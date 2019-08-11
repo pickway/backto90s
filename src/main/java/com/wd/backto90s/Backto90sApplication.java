99f3a03dadbafa79e58b971e199da765333d5f4e
weidong <287917815@qq.com>
2019-08-11 21:10:47 +0800

    The empty project commit

---
+++ src/main/java/com/wd/backto90s/Backto90sApplication.java
@@ -0,0 +1,13 @@
+package com.wd.backto90s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backto90sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backto90sApplication.class, args);
+    }
+
+}
